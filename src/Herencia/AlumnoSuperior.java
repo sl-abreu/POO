6f7d7cd7fcece8605e9d50e7ef41b26ce7f5629a
SLGA <sl.abreu404@gmail.com>
2018-01-25 10:48:02 -0600

    Ejercicio 5

--- src/Herencia/AlumnoSuperior.java
+++ src/Herencia/AlumnoSuperior.java
@@ -12,9 +12,14 @@ public class AlumnoSuperior extends Alumno{
     public AlumnoSuperior() {
         califs=new double[5];
     }
-
     public AlumnoSuperior(String escuelaAnterior, double promAnterior, String nombre, String curp, int mes, int dia, int año, String nombrePadre, String nombreMadre) {
         super(nombre, curp, mes, dia, año, nombrePadre, nombreMadre);
+        this.escuelaAnterior = escuelaAnterior;
+        this.promAnterior = promAnterior;
+        califs=new double[5];
+    }
+    public AlumnoSuperior(String escuelaAnterior, double promAnterior, String nombre, String curp, int mes, int dia, int año, String nombreTutor) {
+        super(nombre, curp, mes, dia, año, nombreTutor);
         this.escuelaAnterior = escuelaAnterior;
         this.promAnterior = promAnterior;
         califs=new double[5];
